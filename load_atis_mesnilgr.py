cdcdd51e087c1f62e4e1429177de3a51d224f0d4
Adam Sadovsky <asadovsky@gmail.com>
2019-08-21 08:05:59 +0200

    cleanups to mesnilgr data loading lib

--- load_atis_mesnilgr.py
+++ load_atis_mesnilgr.py
@@ -1,5 +1,6 @@
 """Utilities for loading mesnilgr's ATIS data."""
 
+from __future__ import print_function
 import cPickle
 
 
@@ -13,20 +14,20 @@ def load_fold(fold):
 def print_fold(fold):
   """Prints ATIS data for the given fold."""
   train, _, test, m = load_fold(fold)
-  w2idx, ne2idx, la2idx = m['words2idx'], m['tables2idx'], m['labels2idx']
+  w2id, ne2id, la2id = m['words2idx'], m['tables2idx'], m['labels2idx']
 
-  idx2w = dict((v, k) for k, v in w2idx.iteritems())
-  idx2ne = dict((v, k) for k, v in ne2idx.iteritems())
-  idx2la = dict((v, k) for k, v in la2idx.iteritems())
+  id2w = dict((v, k) for k, v in w2id.iteritems())
+  id2ne = dict((v, k) for k, v in ne2id.iteritems())
+  id2la = dict((v, k) for k, v in la2id.iteritems())
 
   train_w, train_ne, train_la = train
   test_w, test_ne, test_la = test  # pylint: disable=unused-variable
 
   col = 30
   for w, ne, la in zip(train_w, train_ne, train_la):
-    print 'WORD'.rjust(col), 'TABLE'.rjust(col), 'LABEL'.rjust(col)
+    print('WORD'.rjust(col), 'TABLE'.rjust(col), 'LABEL'.rjust(col))
     for w_i, ne_i, la_i in zip(w, ne, la):
-      print '%s %s %s' % (idx2w[w_i].rjust(col),
-                          idx2ne[ne_i].rjust(col),
-                          idx2la[la_i].rjust(col))
-    print '\n' + '*' * (col * 3 + 2) + '\n'
+      print('{} {} {}'.format(id2w[w_i].rjust(col),
+                              id2ne[ne_i].rjust(col),
+                              id2la[la_i].rjust(col)))
+    print('\n' + '*' * (col * 3 + 2) + '\n')
