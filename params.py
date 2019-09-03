e3ffca22b9a69e6bdacc6c43a40b65ff13184007
Adam Sadovsky <asadovsky@gmail.com>
2019-09-02 21:30:17 -0700

    introduce Params class, a simplified version of the one from tensorflow/lingvo

---
+++ params.py
@@ -0,0 +1,143 @@
+"""A simplified version of tensorflow/lingvo's Params class."""
+
+import re
+import six
+
+
+class _Param:
+  """A parameter."""
+
+  def __init__(self, name, default_value, description):
+    self._name = name
+    self._value = default_value
+    self._description = description
+
+  def __eq__(self, other):
+    # pylint: disable=protected-access
+    return self._name == other._name and self._value == other._value
+
+  def __ne__(self, other):
+    return not self == other
+
+  def to_string(self, nested_depth):
+    """Returns a string representation of this parameter."""
+    if isinstance(self._value, Params):
+      # pylint: disable=protected-access
+      value_str = self._value._to_string(nested_depth)
+    elif isinstance(self._value, six.string_types):
+      value_str = '"%s"' % str(self._value)
+    else:
+      value_str = str(self._value)
+    return '%s%s: %s' % ('  ' * nested_depth, self._name, value_str)
+
+  def set(self, value):
+    self._value = value
+
+  def get(self):
+    return self._value
+
+  def description(self):
+    return self._description
+
+
+class Params:
+  """A data structure for parameters."""
+
+  def __init__(self):
+    self._params = {}
+
+  def __getattr__(self, name):
+    if name == '_params':
+      return self.__dict__[name]
+    try:
+      return self._params[name].get()
+    except KeyError:
+      raise AttributeError(name)
+
+  def __setattr__(self, name, value):
+    if name == '_params':
+      self.__dict__[name] = value
+    else:
+      try:
+        self._params[name].set(value)
+      except KeyError:
+        raise AttributeError(name)
+
+  def __dir__(self):
+    return sorted(self._params.keys())
+
+  def __eq__(self, other):
+    # pylint: disable=protected-access
+    return isinstance(other, Params) and self._params == other._params
+
+  def __ne__(self, other):
+    return not self == other
+
+  def _to_string(self, nested_depth):
+    sorted_param_strs = [
+        v.to_string(nested_depth + 1)
+        for (_, v) in sorted(six.iteritems(self._params))
+    ]
+    return '{\n%s\n%s}' % ('\n'.join(sorted_param_strs), '  ' * nested_depth)
+
+  def __str__(self):
+    return self._to_string(0)
+
+  def __repr__(self):
+    return self._to_string(0)
+
+  def define(self, name, default_value, description):
+    """Defines a parameter.
+
+    Args:
+      name: Parameter name. Must start with a lowercase letter and may only
+          contain lowercase letters, numbers, and underscores.
+      default_value: Default value for this parameter.
+      description: String description of this parameter.
+
+    Raises:
+      AttributeError: If parameter `name` is already defined.
+    """
+    assert (name is not None and
+            isinstance(name, six.string_types) and
+            re.match('^[a-z][a-z0-9_]*$', name) is not None)
+    if name in self._params:
+      raise AttributeError('Parameter is already defined: %s' % name)
+    self._params[name] = _Param(name, default_value, description)
+
+  def _get_nested(self, name):
+    """Returns the specified parameter."""
+    parts = name.split('.')
+    curr = self
+    for i, part in enumerate(parts):
+      try:
+        if not isinstance(curr, Params):
+          raise KeyError
+        param = curr._params[part]  # pylint: disable=protected-access
+        if i == len(parts) - 1:
+          return param
+        curr = param.get()
+      except KeyError:
+        raise AttributeError('.'.join(parts[:i+1]))
+    return None
+
+  def get(self, name):
+    """Returns the value of the specified parameter."""
+    return self._get_nested(name).get()
+
+  def set(self, **kwargs):
+    """Sets the specified parameters."""
+    for name, value in six.iteritems(kwargs):
+      self._get_nested(name).set(value)
+
+  def has(self, name):
+    """Returns True if the specified parameter exists."""
+    try:
+      self._get_nested(name)
+      return True
+    except AttributeError:
+      return False
+
+  def description(self, name):
+    """Returns the description of the specified parameter."""
+    return self._get_nested(name).description()
---
+++ params_test.py
@@ -0,0 +1,71 @@
+"""Tests for Params class."""
+
+import unittest
+
+from params import Params
+
+
+class TestParams(unittest.TestCase):
+  """Tests for Params class."""
+
+  def test_equals(self):
+    """Tests equality methods."""
+    a = Params()
+    b = Params()
+    self.assertEqual(a, b)
+    a.define('x', 0, '')
+    self.assertNotEqual(a, b)
+    b.define('x', 0, '')
+    self.assertEqual(a, b)
+    a.x = 1
+    self.assertNotEqual(a, b)
+    a.x = 0
+    self.assertEqual(a, b)
+
+  def test_get_set_has(self):
+    """Tests get, set, and has methods."""
+    p = Params()
+    self.assertFalse(p.has('x'))
+    self.assertRaises(AttributeError, lambda: p.x)
+    self.assertRaises(AttributeError, lambda: p.get('x'))
+    p.define('x', 0, '')
+    self.assertTrue(p.has('x'))
+    self.assertEqual(p.x, 0)
+    self.assertEqual(p.get('x'), 0)
+    p.x = 1
+    self.assertTrue(p.has('x'))
+    self.assertEqual(p.x, 1)
+    self.assertEqual(p.get('x'), 1)
+    p.set(x=2)
+    self.assertTrue(p.has('x'))
+    self.assertEqual(p.x, 2)
+    self.assertEqual(p.get('x'), 2)
+
+  def test_description(self):
+    """Tests description method."""
+    p = Params()
+    p.define('x', 0, 'x desc')
+    p.define('y', 1, '')
+    self.assertEqual(p.description('x'), 'x desc')
+    self.assertEqual(p.description('y'), '')
+    self.assertRaises(AttributeError, lambda: p.description('z'))
+
+  def test_nested(self):
+    """Tests for nested Params."""
+    a = Params()
+    a.define('x', 0, '')
+    self.assertEqual(str(a), '{\n  x: 0\n}')
+    b = Params()
+    a.define('y', b, '')
+    b.define('z', 1, '')
+    self.assertEqual(str(a), '{\n  x: 0\n  y: {\n    z: 1\n  }\n}')
+    self.assertRaises(AttributeError, lambda: a.get('w'))
+    self.assertEqual(a.get('x'), 0)
+    self.assertRaises(AttributeError, lambda: a.get('x.y'))
+    self.assertRaises(AttributeError, lambda: a.get('y.w'))
+    self.assertEqual(a.get('y.z'), 1)
+    self.assertRaises(AttributeError, lambda: a.get('y.z.w'))
+
+
+if __name__ == '__main__':
+  unittest.main()
