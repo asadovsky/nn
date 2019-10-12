a8f79d8fcc70c0c149ed71f72480f5ecdaacf1e7
Adam Sadovsky <asadovsky@gmail.com>
2019-10-11 21:29:08 -0700

    update linear_regression.py to use TF 2.x APIs

--- linear_regression.py
+++ linear_regression.py
@@ -1,21 +1,20 @@
 """Linear regression examples."""
 
-# Note, this module assumes TensorFlow 1.x, and does not support 2.x.
-# pylint: disable=no-member
-
 from __future__ import print_function
 
 import numpy as np
 import tensorflow as tf
 
 np.random.seed(0)
-tf.set_random_seed(0)
+tf.random.set_seed(0)
 
-X_VALS = np.linspace(0, 50, 50) + np.random.uniform(-4, 4, 50)
-Y_VALS = np.linspace(0, 50, 50) + np.random.uniform(-4, 4, 50)
+X_VALS = (np.linspace(0, 50, 50) +
+          np.random.uniform(-4, 4, 50)).astype(np.float32)
+Y_VALS = (np.linspace(0, 50, 50) +
+          np.random.uniform(-4, 4, 50)).astype(np.float32)
 N = len(Y_VALS)
 
-TRAIN_EPOCHS = 1000
+TRAIN_EPOCHS = 2000
 LOG_EPOCHS = 100
 
 
@@ -27,8 +26,7 @@ def print_vars(estimator):
 
 def input_fn(mode):
   """Input function for estimators."""
-  d = tf.data.Dataset.from_tensor_slices(
-      ({"x": tf.to_float(X_VALS)}, tf.to_float(Y_VALS)))
+  d = tf.data.Dataset.from_tensor_slices(({"x": X_VALS}, Y_VALS))
   d = d.batch(1)
   if mode == tf.estimator.ModeKeys.TRAIN:
     d = d.repeat()
@@ -39,13 +37,14 @@ def model_fn(features, labels, mode):
   """Model function for estimators."""
   x = tf.reshape(features["x"], [-1, 1])
   y = labels
-  w = tf.get_variable(
-      "w", shape=[1, 1], initializer=tf.contrib.layers.xavier_initializer())
-  b = tf.get_variable("b", shape=[], initializer=tf.zeros_initializer())
+  w = tf.Variable(tf.initializers.GlorotUniform()(shape=[1, 1]), name="w")
+  b = tf.Variable(tf.zeros(shape=[]), name="b")
+  trainable_variables = [w, b]
   pred = tf.reshape(tf.matmul(x, w) + b, [-1])
-  loss = tf.reduce_sum((pred - y) ** 2) / tf.to_float(tf.size(pred))
-  train_op = tf.train.AdamOptimizer().minimize(
-      loss, global_step=tf.train.get_global_step())
+  loss = tf.reduce_sum((pred - y) ** 2) / tf.cast(tf.size(pred), tf.float32)
+  optimizer = tf.keras.optimizers.Adam()
+  optimizer.iterations = tf.compat.v1.train.get_or_create_global_step()
+  train_op = optimizer.get_updates(loss, trainable_variables)[0]
   return tf.estimator.EstimatorSpec(
       mode=mode, predictions=pred, loss=loss, train_op=train_op)
 
@@ -79,26 +78,30 @@ def train_linear_regressor():
 
 def train_ad_hoc():
   """Trains linear regression model using ad hoc training code."""
-  tf.reset_default_graph()
-  x = tf.placeholder(tf.float32, shape=[None, 1])
-  y = tf.placeholder(tf.float32, shape=[None])
-  w = tf.get_variable(
-      "w", shape=[1, 1], initializer=tf.contrib.layers.xavier_initializer())
-  b = tf.get_variable("b", shape=[], initializer=tf.zeros_initializer())
-  pred = tf.reshape(tf.matmul(x, w) + b, [-1])
-  loss = tf.reduce_sum((pred - y) ** 2) / tf.to_float(tf.size(pred))
-  train_op = tf.train.AdamOptimizer().minimize(loss)
+  w = tf.Variable(tf.initializers.GlorotUniform()(shape=[1, 1]), name="w")
+  b = tf.Variable(tf.zeros(shape=[]), name="b")
+  trainable_variables = [w, b]
+  optimizer = tf.keras.optimizers.Adam()
 
-  with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    eval_feed_dict = {x: X_VALS.reshape([-1, 1]), y: Y_VALS}
-    for epoch in range(TRAIN_EPOCHS):
-      if epoch % LOG_EPOCHS == 0:
-        print("epoch={} loss={} w={} b={}".format(
-            epoch, *sess.run([loss, w, b], feed_dict=eval_feed_dict)))
-      for (x_val, y_val) in zip(X_VALS, Y_VALS):
-        sess.run(train_op,
-                 feed_dict={x: np.reshape(x_val, [-1, 1]),
-                            y: np.reshape(y_val, [-1])})
-    print("loss={} w={} b={}".format(
-        *sess.run([loss, w, b], feed_dict=eval_feed_dict)))
+  def _loss(x, y):
+    pred = tf.reshape(tf.matmul(x, w) + b, [-1])
+    return tf.reduce_sum((pred - y) ** 2) / tf.cast(tf.size(pred), tf.float32)
+
+  def _loss_and_vars_str():
+    return "loss={:.4f} w={} b={}".format(
+        _loss(X_VALS.reshape([-1, 1]), Y_VALS), w.numpy(), b.numpy())
+
+  @tf.function
+  def _train_step(x, y):
+    with tf.GradientTape() as tape:
+      loss = _loss(x, y)
+    grads = tape.gradient(loss, trainable_variables)
+    optimizer.apply_gradients(zip(grads, trainable_variables))
+
+  for epoch in range(TRAIN_EPOCHS):
+    if epoch % LOG_EPOCHS == 0:
+      print("epoch={} {}".format(epoch, _loss_and_vars_str()))
+    for x_val, y_val in zip(X_VALS, Y_VALS):
+      _train_step(np.reshape(x_val, [-1, 1]), np.reshape(y_val, [-1]))
+
+  print(_loss_and_vars_str())
