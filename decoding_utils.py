99bdbe24e02042255966abe7898da2094a645427
Adam Sadovsky <asadovsky@gmail.com>
2019-10-14 17:18:52 -0700

    switch from single to double quotes in util libs

--- decoding_utils.py
+++ decoding_utils.py
@@ -8,9 +8,9 @@ import tensorflow as tf
 
 def _split_iob_tag(tag: Text) -> Tuple[Text, Union[Text, None]]:
   """Handles tags <pad>, <unk>, O, B-foo, and I-foo."""
-  if tag[:2] not in ['B-', 'I-']:
+  if tag[:2] not in ["B-", "I-"]:
     return tag, None
-  return tuple(tag.split('-', maxsplit=2))
+  return tuple(tag.split("-", maxsplit=2))
 
 
 def iob_transition_params(tags: List[Text]) -> Any:
@@ -20,7 +20,7 @@ def iob_transition_params(tags: List[Text]) -> Any:
     i_type, i_name = _split_iob_tag(i_tag)
     for j, j_tag in enumerate(tags):
       j_type, j_name = _split_iob_tag(j_tag)
-      if ((i_type == 'O' and j_type == 'I') or
-          (i_type in ['B', 'I'] and j_type == 'I' and i_name != j_name)):
+      if ((i_type == "O" and j_type == "I") or
+          (i_type in ["B", "I"] and j_type == "I" and i_name != j_name)):
         mat[i][j] = -np.inf
   return tf.constant(mat, dtype=tf.float32)
